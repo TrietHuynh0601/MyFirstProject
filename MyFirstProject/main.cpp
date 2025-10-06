04c9e595b56c3144b16f3b93a6894cf2b26ae7ed
Triet Huynh <triethuynh0601@gmail.com>
2025-10-06 10:39:53 +0700

    Update main.cpp with new code

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
-int main(int argc, char const *argv[])
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include <iomanip>
+using namespace std;
+int main(){
+    int num1, num2;
+    double num3;
+    cin >> num1 >> num2 >> num3;
+    float avg = (num1+num2+num3)/3.0;
+    cout << avg;
+}    
